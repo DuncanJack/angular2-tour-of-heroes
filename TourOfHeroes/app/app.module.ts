b0aeb80831895adbfd205086ae98951be2485f21
duncan.jack <duncan.jack@live.co.uk>
2016-09-24 13:38:16 +0100

    6 Completed 'Add a Dashboard'

--- TourOfHeroes/app/app.module.ts
+++ TourOfHeroes/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { routing } from './app.routing';
+
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
-
-import { routing } from './app.routing';
 
 @NgModule({
     imports: [
@@ -17,6 +18,7 @@ import { routing } from './app.routing';
     ],
     declarations: [
         AppComponent,
+        DashboardComponent,
         HeroDetailComponent,
         HeroesComponent
     ],
